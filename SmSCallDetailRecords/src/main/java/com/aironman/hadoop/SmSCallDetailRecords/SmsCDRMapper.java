faf5959a18ca117fa43026371bc8e0d5d48ffa29
Alonso <alonsoir@gmail.com>
2014-04-07 12:14:19 +0200

    a bit changed

--- SmSCallDetailRecords/src/main/java/com/aironman/hadoop/SmSCallDetailRecords/SmsCDRMapper.java
+++ SmSCallDetailRecords/src/main/java/com/aironman/hadoop/SmSCallDetailRecords/SmsCDRMapper.java
@@ -23,6 +23,10 @@ public class SmsCDRMapper extends Mapper<LongWritable, Text, Text, IntWritable> 
 	private Text status = new Text();
 	private final static IntWritable addOne = new IntWritable(1);
 
+	static enum CDRCounter {
+		NonSMSCDR;
+	};
+
 	/**
 	 * Returns the SMS status code and its count
 	 */
@@ -32,11 +36,13 @@ public class SmsCDRMapper extends Mapper<LongWritable, Text, Text, IntWritable> 
 		// 655209;1;796764372490213;804422938115889;6 is the Sample record
 		// format
 		String[] line = value.toString().split(";");
-		
+
 		// If record is of SMS CDR
 		if (Integer.parseInt(line[1]) == 1) {
 			status.set(line[4]);
 			context.write(status, addOne);
-		}
+		}else {// CDR record is not of type SMS so increment the counter
+		      context.getCounter(CDRCounter.NonSMSCDR).increment(1);
+	    }
 	}
 }
--- SmSCallDetailRecords/src/test/java/com/aironman/hadoop/SmSCallDetailRecords/SMSCDRMapperReducerTest.java
+++ SmSCallDetailRecords/src/test/java/com/aironman/hadoop/SmSCallDetailRecords/SMSCDRMapperReducerTest.java
@@ -1,8 +1,11 @@
 package com.aironman.hadoop.SmSCallDetailRecords;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -12,18 +15,28 @@ import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.aironman.hadoop.SmSCallDetailRecords.SmsCDRMapper.CDRCounter;
+
 public class SMSCDRMapperReducerTest {
 
 	MapDriver<LongWritable, Text, Text, IntWritable> mapDriver;
 	ReduceDriver<Text, IntWritable, Text, IntWritable> reduceDriver;
 	MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver;
+	
+	Configuration conf = new Configuration();
 
 	@Before
 	public void setUp() {
+		
 		SmsCDRMapper mapper = new SmsCDRMapper();
 		SmsCDRReducer reducer = new SmsCDRReducer();
+		
 		mapDriver = MapDriver.newMapDriver(mapper);
-		;
+		//Passing arguments for Testing, but its deprecated
+		conf.set("myParameter1", "20");
+		conf.set("myParameter2", "23");
+		mapDriver.setConfiguration(conf);
+		
 		reduceDriver = ReduceDriver.newReduceDriver(reducer);
 		mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
 	}
@@ -34,6 +47,17 @@ public class SMSCDRMapperReducerTest {
 				"655209;1;796764372490213;804422938115889;6"));
 		mapDriver.withOutput(new Text("6"), new IntWritable(1));
 		mapDriver.runTest();
+
+	}
+
+	@Test
+	public void testMapperWillFail() throws Exception {
+		mapDriver.withInput(new LongWritable(), new Text(
+				"655209;0;796764372490213;804422938115889;6"));
+		// mapDriver.withOutput(new Text("6"), new IntWritable(1));
+		mapDriver.runTest();
+		assertEquals("Expected 1 counter increment", 1,
+				mapDriver.getCounters().findCounter(CDRCounter.NonSMSCDR).getValue());
 	}
 
 	@Test
